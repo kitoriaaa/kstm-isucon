d2321b5fdaa770cab8ba201693f7612b4dec3f26
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-04-30 01:10:23 +0900

    Add: unicorn log output setting

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -3,3 +3,5 @@ preload_app true
 pid './unicorn.pid'
 listen 8080
 
+stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
